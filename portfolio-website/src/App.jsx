9077b46aa85cf9b2309bbc0c73d0ef1403002474
Yasir <yasirrjami@hotmail.com>
2024-07-25 03:43:59 -0600

    Update images

--- portfolio-website/src/App.jsx
+++ portfolio-website/src/App.jsx
@@ -2,9 +2,6 @@ import Header from "./Header.jsx"
 import Footer from "./Footer.jsx"
 import About from "./About.jsx"
 import ProjectCard from "./ProjectCard.jsx"
-import processSimulatorImage from "./assets/process_sim.png"
-import paddleImage from "./assets/paddle.png"
-import shiftImage from "./assets/ShiftSmart.png"
 
 function App() {
   return(
@@ -17,21 +14,21 @@ function App() {
         projectDescription="Two-player web game made with HTML and JavaScript. 
         Play the game by hitting the ball past your opponent's paddle. 
         The winner is the one who scores 9 goals first."
-        projectImage={paddleImage}
+        projectImage="/portfolio-website/src/assets/paddle.png"
       />
       <ProjectCard 
         projectName="ShiftSmart" 
         projectTitleColor="#b87333" 
         projectDescription="A Java mobile application used to schedule shifts for employees. 
         Developed in a four-person team while following Agile methodology for project management."
-        projectImage={processSimulatorImage}
+        projectImage="/portfolio-website/src/assets/ShiftSmart.png"
       />
       <ProjectCard 
         projectName="Linux Process Simulator" 
         projectTitleColor="#b87333"
         projectDescription="Program that simulates the queueing and running of processes on Unix-like systems. 
         A log file recording the execution is used to generate an image like the one below."
-        projectImage={paddleImage}
+        projectImage="/portfolio-website/src/assets/ProcessSimulatorGraph.png"
       />
       <Footer />
     </>
--- portfolio-website/src/ProjectCard.jsx
+++ portfolio-website/src/ProjectCard.jsx
@@ -1,14 +1,25 @@
 import PropTypes from "prop-types";
 import "./styles/ProjectCard.css"
-import projectImage from "./assets/paddle.png"
+import paddleImage from "./assets/paddle.png"
+import shiftImage from "./assets/ShiftSmart.png"
+import processSimulatorImage from "./assets/ProcessSimulatorGraph.png"
 
 function ProjectCard(props){
+  let imageSource = paddleImage; // default
+
+  if (props.projectImage === shiftImage) {
+    imageSource = shiftImage;
+  }
+  else if (props.projectImage === processSimulatorImage){
+    imageSource = processSimulatorImage;
+  }
+
   return(
     <div className = "project-card">
       <h2 style={{color: props.projectTitleColor}}>{props.projectName}</h2>
       <hr></hr>
       <h4 className="project-description">{props.projectDescription}</h4>
-      <img className="project-image" src={projectImage} alt="project image"></img>
+      <img className="project-image" src={imageSource} alt="image"></img>
     </div>
   );
 }
@@ -22,7 +33,7 @@ ProjectCard.proptypes = {
 
 ProjectCard.defaultProps = {
   projectName: "Project",
-  projectImage: "Image Link",
+  projectImage: "Image",
   projectTitleColor: "#b87333",
   projectDescription: "Description",
 }
